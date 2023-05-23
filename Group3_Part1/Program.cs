864151cc64a03246b366ade14d4d2f0dba742540
KrystynaHurieieva <Krystyna.hurieieva@infopulse.com>
2023-05-23 18:47:13 +0300

    Add project files.

---
+++ Group3_Part1/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Group3_Part1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!"); // one row comment
+            ///*
+            //Ingeral types byte, int, long, char, short 
+            //float double 2.65 
+            //decimal 
+            //bool 
+            //string 
+            // */
+            //int r_ = 6;
+            //int a = 5, b = 3, c = 1;
+            //double rr = 2.56;
+            //float rrr = 2.56f;
+            //decimal salaryInMyCompany, yy; //  79,228,162,514,264,337,593,543,950,335
+            //char _t;
+            //salaryInMyCompany = Decimal.One;
+            //yy = 256.31m;
+
+            //Console.WriteLine("Hello, World!");
+            //Console.WriteLine(salaryInMyCompany);
+            //var test = Console.ReadLine();
+
+            //Console.WriteLine(test);
+            //var test1 = 456789;
+            //salaryInMyCompany = 45567m;
+
+            //const double pi = 3.14159;
+            //float float_ = 234;
+            //double double_ = 1234;
+
+            //double_ = float_;
+            //float_ = (float)double_;
+
+            // ToBoolean ToByte
+
+
+            //Console.WriteLine("Input number: ");
+            //var inp = Console.ReadLine();
+            //int test = Convert.ToInt32(inp);
+
+            //Console.WriteLine("Your number :");
+            //Console.WriteLine(test);
+
+            //// + - * / % ++ -- 
+
+            //var test1 = test + 10;
+            //var test2 = test - 10;
+            //var test3 = test * 10;
+            //double test4 = (Convert.ToDouble(test) / 10);
+            //var test5 = test % 2;
+            //test++;
+            //var test6 = test;
+            //test--;
+            //var test7 = test;
+
+
+            //Console.WriteLine(test1);
+            //Console.WriteLine(test2);
+            //Console.WriteLine(test3);
+            //Console.WriteLine(test4);
+            //Console.WriteLine("парне? ");
+            //Console.WriteLine( test5.ToString());
+            //Console.WriteLine(test6);
+            //Console.WriteLine(test7);
+
+
+            // && || !
+            bool a = true;
+            bool b = false;
+            Console.WriteLine(a && b);
+            Console.WriteLine(a  || b);
+            Console.WriteLine(!(a && b));
+            // == != > < >= <=
+        }
+    }
+}
