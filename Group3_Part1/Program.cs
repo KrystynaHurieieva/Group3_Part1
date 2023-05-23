465e484809f5792e769ec6b187df25ba7f33f6fb
KrystynaHurieieva <Krystyna.hurieieva@infopulse.com>
2023-05-23 18:50:15 +0300

    test message

--- Group3_Part1/Program.cs
+++ Group3_Part1/Program.cs
@@ -4,7 +4,8 @@
     {
 
         static void Main(string[] args)
-        {
+        { 
+            /// test commit 
             //Console.WriteLine("Hello, World!"); // one row comment
             ///*
             //Ingeral types byte, int, long, char, short 
