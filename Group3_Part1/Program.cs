f712870bfee3e7118c2770d81b0e6083515ac21e
KrystynaHurieieva <Krystyna.hurieieva@infopulse.com>
2023-05-30 18:55:08 +0300

    Lecture 4

--- Group3_Part1/Program.cs
+++ Group3_Part1/Program.cs
@@ -1,8 +1,9 @@
-﻿namespace Group3_Part1
+﻿using System.Reflection;
+
+namespace Group3_Part1
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
             {//var a = 3; 
@@ -89,53 +90,126 @@
                 //var res = a > 10 ? "a>10" : "a<10";
                 //Console.WriteLine(res);
             }
-            //var a = 100;
-            //while (a < 10) 
-            //{
-            //    Console.WriteLine($"a={a}");
-            //    a++;
-            //}
+            {//var a = 100;
+             //while (a < 10) 
+             //{
+             //    Console.WriteLine($"a={a}");
+             //    a++;
+             //}
 
-            //for(int i = 100; i < 10; i++)
-            //{
-            //    Console.WriteLine($"i={i}");
-            //}
+                //for(int i = 100; i < 10; i++)
+                //{
+                //    Console.WriteLine($"i={i}");
+                //}
 
-            //do
-            //{
-            //    Console.WriteLine($"a={a}");
-            //    a++;
-            //}
-            //while (a < 100);
+                //do
+                //{
+                //    Console.WriteLine($"a={a}");
+                //    a++;
+                //}
+                //while (a < 100);
 
-            // break;
+                // break;
 
-            //int a = 10;
+                //int a = 10;
 
-            //while (a < 20)
-            //{
-            //    Console.WriteLine($"a = {a}");
-            //    a++;
+                //while (a < 20)
+                //{
+                //    Console.WriteLine($"a = {a}");
+                //    a++;
 
-            //    if (a > 15)
-            //        continue;
+                //    if (a > 15)
+                //        continue;
 
-            //    Console.WriteLine($"hello");
-            //}
-            var str = "test";
-            for (; ; )
+                //    Console.WriteLine($"hello");
+                //}
+                //var str = "test";
+                //for (; ; )
+                //{
+                //    str += " hello"; 
+                //    Console.WriteLine($"{str}");
+                //}
+                //while (true)
+                //{
+                //    str += " hello";
+                //    Console.WriteLine($"{str}");
+                //}
+                // int, bool, double, float, decimal, byte, char
+                // string object
+            }
+            //int a = 1;
+            //int b = 2;
+            //int result = 100;
+            //SumOfNubers(ref a, ref b, out result, out int res1);
+            //Console.WriteLine($"{a}, {b}");
+            ////SumOfNubers(3, 4); is as 
+            {//Console.WriteLine("----------------------------");
+             //var test1 = FindMax(1, 2);
+             //Console.WriteLine(test1);
+             //Console.WriteLine(FindMax(-1, 2));
+             //Console.WriteLine("-------------Factorial---------------");
+             //Console.WriteLine(Factorial(5));
+             //Console.WriteLine(Factorial(13));
+             //Console.WriteLine(Factorial(50));
+            }
+            //data_typep[] name;
+            int[] arr1 = new int[10];
+            arr1[0] = 10; arr1[1] = 20; arr1[2] = 30;
+
+            int[] arr2 = { 12, 13, 14, 15 };
+
+            int[] arr3 = new int[5] { 1, 4, 2, 6, 3 };
+
+            int[] arr4 = new int[] { 32, 1, 63, 74, 84, 38 };
+
+            var test = new int[10];
+
+            for(int i = 0; i < 5; i++)
             {
-                str += " hello"; 
-                Console.WriteLine($"{str}");
-
+                Console.WriteLine(arr3[i]);
             }
 
-            while (true)
+            foreach(var el in arr3)
             {
-                str += " hello";
-                Console.WriteLine($"{str}");
+                Console.WriteLine(el);
             }
 
+        }
+        //Access_specifier return_type MethodName (parameters)
+        //{
+        //    MethodBody 
+        //}
+
+        public static void SumOfNubers(ref int a, ref int b, out int res, out int res1)
+        {
+            a += 10;
+            b += 100;
+            res = a + b;
+            res1 = b;
+            Console.WriteLine(res);
+            Console.WriteLine($"{a}, {b}");
+        }
+
+        public static int FindMax(int a, int b)
+        {
+            int result;
+            result = a > b ? a : b;
+            return result;
+            // return a > b ? a : b;
+        }
+        public static ulong Factorial(ulong num)
+        {
+            if (num < 1)
+                return 100;
+
+            ulong result;
+            if (num == 1)
+                return 1;
+            else
+            {
+                result = Factorial(num - 1) * num;
+                return result;
+            }
         }
     }
 }
